e0d3ff0fd87aac69937e8bb17a8548679b3013b6
Yihan Li <Liyihan11unique@outlook.com>
2026-03-30 16:52:44 +0800

    init commit

---
+++ composeApp/src/androidMain/kotlin/com/ikkoaudio/androidstartupoptimizationframework/Platform.kt
@@ -0,0 +1,9 @@
+package com.ikkoaudio.androidstartupoptimizationframework
+
+import android.os.Build
+
+class AndroidPlatform {
+    val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+fun getPlatform() = AndroidPlatform()
